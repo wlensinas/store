980f03adf1e3320b15d215574da2a68f8565a021
Walter Lensinas <wlensinas@buenosaires.gob.ar>
2021-05-19 20:13:50 -0300

    config en archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
+import { environments } from './environments';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
       isGlobal: true,
     }),
     HttpModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
+    //@Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
   ) {}
   getHello(): any {
-    //console.log(this.tasks);
-    return `Hello ${this.apiKey}`;
+    const apiKey = this.config.get<string>('API_KEY');
+    const dbName = this.config.get<string>('DATABASE_NAME');
+    return `api: ${apiKey} db: ${dbName}`;
     //return this.tasks;
   }
 }
