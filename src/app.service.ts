bab4a25c8fd539056cb6630c5fb32558115fdb10
Walter Lensinas <wlensinas@buenosaires.gob.ar>
2021-05-19 12:18:39 -0300

    added useFactory

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,13 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
-  getHello(): string {
-    return `Hello ${this.apiKey}`;
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
+  getHello(): any {
+    //console.log(this.tasks);
+    //return `Hello ${this.apiKey}`;
+    return this.tasks;
   }
 }
