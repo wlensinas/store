0c6064fd8518a5d2488656a77d35fdcfb7d9eebb
Walter Lensinas <wlensinas@buenosaires.gob.ar>
2021-05-19 23:03:50 -0300

    added swagger

--- src/users/dtos/customer.dto.ts
+++ src/users/dtos/customer.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+//import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 
 export class CreateCustomerDto {
   @IsString()
--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,5 +1,6 @@
 import { IsString, IsNotEmpty, IsEmail, Length } from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+//import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsString()
